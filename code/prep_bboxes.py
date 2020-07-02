d8e22b060d5aff1645e100fa6cfd6e09680b6cb6
hencockable <hendrikfelderhoff@googlemail.com>
2020-07-02 21:32:15 +0200

    autopush

--- code/prep_bboxes.py
+++ code/prep_bboxes.py
@@ -1,15 +1,26 @@
-import argparse, os
+import argparse, os, sys
 import pandas as pd
 
 
 # Argument parser
 parser = argparse.ArgumentParser(description='Pre-processing of bbox annotations to fit Hopenet.')
 parser.add_argument('--file_path', dest='file_path', help='Path to bbox file', type=str, required=True)
+parser.add_argument("--out_dir", dest="out_dir", help="Output directory", type=str, required=True)
 args = parser.parse_args()
 
-# get path to bbox .csv file
+# get args
 path = args.file_path
-name = os.path.splitext(path)[0]
+outdir = args.out_dir
+
+if not os.path.exists(outdir):
+    os.makedirs(outdir)
+
+if not os.path.exists(path):
+    sys.exit('File does not exist')
+
+# get file name
+name = os.path.splitext(os.path.basename(path))[0]
+out_name = outdir + name + "_hopenet.csv"
 
 # read bbox .csv file
 df = pd.read_csv(path)
@@ -18,7 +29,8 @@ df = pd.read_csv(path)
 out = df[["frame_num", "bb_x1", "bb_y1", "bb_x2", "bb_y2", "score"]].copy()
 
 # save as new .csv file
-out.to_csv("{}_hopenet.csv".format(name), index=False)
+out.to_csv(out_name, index=False)
+print("Saved", out_name)
 
 
 
